ea54563af8e22c80913acf01c5bad719703ddab8
jbreure <devnull@localhost>
2010-05-04 14:46:04 +1000

    removed legacy package

--- src/org/weborganic/flint/legacy/LegacyLuceneVersion.java
+++
@@ -1,7 +0,0 @@
-package org.weborganic.flint.legacy;
-
-import org.apache.lucene.util.Version;
-
-public class LegacyLuceneVersion {
-  public final static Version VERSION = Version.LUCENE_23;
-}
--- src/org/weborganic/flint/legacy/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Legacy classes from Flint 0.x - will be obsolete or moved.
- */
-package org.weborganic.flint.legacy;
