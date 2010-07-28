0fdcf6c7a28b5e85aac6f1dce7e621f3910b01fc
clauret <devnull@localhost>
2010-07-28 16:27:58 +1000

    Annotation for methods and classes in beta

---
+++ src/org/weborganic/flint/util/Beta.java
@@ -0,0 +1,11 @@
+package org.weborganic.flint.util;
+
+/**
+ * This annotation indicates that the annotated symbol (field, method, class) may change in 
+ * subsequent releases. 
+ * 
+ * @author Christophe Lauret
+ */
+public @interface Beta {
+
+}
