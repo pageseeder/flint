1e27c2086ff4f7217b640e4c5bdaf536ec33d335
christophe <devnull@localhost>
2010-09-22 16:08:38 +1000

    Added new search parameter: numeric range

--- src/org/weborganic/flint/query/package-info.java
+++ src/org/weborganic/flint/query/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Classes used specifically for querying.
+ * 
+ * This package contains {@link org.weborganic.flint.query.SearchParameter} implementations, which can be used to construct queries.
  */
 package org.weborganic.flint.query;
