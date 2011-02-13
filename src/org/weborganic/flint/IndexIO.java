5deb5f246d92a47067ce8332ccb7997a723f1532
ccai <devnull@localhost>
2011-02-14 09:27:16 +1100

    Ensure all files can write before return IndexIO instance.

--- src/org/weborganic/flint/IndexIO.java
+++ src/org/weborganic/flint/IndexIO.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
@@ -20,10 +21,12 @@ import org.weborganic.flint.content.DeleteRule;
 /**
  * Provides a set of utility methods to deal with IO operations on an Index.
  * 
- * <p>This class is useful to centralise all operations on an index because it will create one
- * writer and share it with other classes if needed.
+ * <p>
+ * This class is useful to centralise all operations on an index because it will
+ * create one writer and share it with other classes if needed.
  * 
- * <p>This is a lower level API.
+ * <p>
+ * This is a lower level API.
  * 
  * @author Jean-Baptiste Reure
  * @version 26 February 2010
@@ -31,7 +34,7 @@ import org.weborganic.flint.content.DeleteRule;
 public abstract class IndexIO {
 
   /**
-   * Describes the state of an index. 
+   * Describes the state of an index.
    */
   static enum State {
     /** The index is in a clean state, ready to use. */
@@ -40,7 +43,10 @@ public abstract class IndexIO {
     NEEDS_REOPEN,
     /** The index has been modified but changes have not been committed. */
     NEEDS_COMMIT,
-    /** The index has been modified and changes have been committed, but it is not in an optimal state. */
+    /**
+     * The index has been modified and changes have been committed, but it is
+     * not in an optimal state.
+     */
     NEEDS_OPTIMISE
   };
 
@@ -57,7 +63,8 @@ public abstract class IndexIO {
   /**
    * Sole constructor.
    * 
-   * @param index The index on which IO operations will occur. 
+   * @param index
+   *          The index on which IO operations will occur.
    */
   IndexIO(Index index) {
     this.indexID = index.getIndexID();
@@ -66,7 +73,7 @@ public abstract class IndexIO {
   /**
    * Returns the ID of the index used for this class.
    * 
-   * @return the ID of the index used for this class.  
+   * @return the ID of the index used for this class.
    */
   public final String indexID() {
     return this.indexID;
@@ -75,14 +82,16 @@ public abstract class IndexIO {
   /**
    * Commit any changes if the state of the index requires it.
    * 
-   * @throws IndexException should any error be thrown by Lucene while committing.
+   * @throws IndexException
+   *           should any error be thrown by Lucene while committing.
    */
   public abstract void maybeCommit() throws IndexException;
 
   /**
    * Commit any changes if the state of the index requires it.
    * 
-   * @throws IndexException should any error be thrown by Lucene while optimising.
+   * @throws IndexException
+   *           should any error be thrown by Lucene while optimising.
    */
   public abstract void maybeOptimise() throws IndexException;
 
@@ -91,44 +100,57 @@ public abstract class IndexIO {
    * 
    * @return <code>true</code> if the indexed could be scheduled for clearing;
    *         <code>false</code>
-   * @throws IndexException should any error be thrown by Lucene.
-   * @throws UnsupportedOperationException If this implementation is read only.
+   * @throws IndexException
+   *           should any error be thrown by Lucene.
+   * @throws UnsupportedOperationException
+   *           If this implementation is read only.
    */
   public abstract boolean clearIndex() throws IndexException, UnsupportedOperationException;
 
   /**
-   * Delete the documents defined in the delete rule as soon as possible (asynchronously).
+   * Delete the documents defined in the delete rule as soon as possible
+   * (asynchronously).
    * 
-   * @param rule the rule to identify the items to delete
+   * @param rule
+   *          the rule to identify the items to delete
    * @return <code>true</code> if the item could be be scheduled for deletion;
    *         <code>false</code>
-   * @throws IndexException should any error be thrown by Lucene.
-   * @throws UnsupportedOperationException If this implementation is read only.
+   * @throws IndexException
+   *           should any error be thrown by Lucene.
+   * @throws UnsupportedOperationException
+   *           If this implementation is read only.
    */
   public abstract boolean deleteDocuments(DeleteRule rule) throws IndexException, UnsupportedOperationException;
 
   /**
-   * Update the documents defined in the delete rule as soon as possible (asynchronously).
+   * Update the documents defined in the delete rule as soon as possible
+   * (asynchronously).
    * 
-   * <p>It is not possible to update an item in Lucene, instead it is first deleted then inserted 
-   * again.
+   * <p>
+   * It is not possible to update an item in Lucene, instead it is first deleted
+   * then inserted again.
    * 
-   * @param rule the rule to identify the items to delete before update.
-   * @param documents the list of documents to replace with.
+   * @param rule
+   *          the rule to identify the items to delete before update.
+   * @param documents
+   *          the list of documents to replace with.
    * @return <code>true</code> if the item could be be scheduled for update;
    *         <code>false</code>
-   * @throws IndexException should any error be thrown by Lucene
-   * @throws UnsupportedOperationException If this implementation is read only.
+   * @throws IndexException
+   *           should any error be thrown by Lucene
+   * @throws UnsupportedOperationException
+   *           If this implementation is read only.
    */
-  public abstract boolean updateDocuments(DeleteRule rule, List<Document> documents) 
-    throws IndexException, UnsupportedOperationException;
+  public abstract boolean updateDocuments(DeleteRule rule, List<Document> documents) throws IndexException, UnsupportedOperationException;
 
   /**
    * Returns the index searcher for the index.
    * 
-   * <p>Note: when a searcher is booked it must be released using {@link #releaseSearcher(IndexSearcher)}.
+   * <p>
+   * Note: when a searcher is booked it must be released using
+   * {@link #releaseSearcher(IndexSearcher)}.
    * 
-   * @return The index searcher. 
+   * @return The index searcher.
    * 
    * @throws IOException
    */
@@ -148,30 +170,46 @@ public abstract class IndexIO {
   /**
    * Closes the writer on this index.
    * 
-   * @throws IndexException Wrapping an {@link CorruptIndexException} or an {@link IOException}.
+   * @throws IndexException
+   *           Wrapping an {@link CorruptIndexException} or an
+   *           {@link IOException}.
    */
   public abstract void stop() throws IndexException;
 
   /**
    * Generate the appropriate IndexIO implementation to use.
    * 
-   * <p>This method will try to return a read/write IndexIO instance if possible; otherwise it will
-   * return a read only instance. 
+   * <p>
+   * This method will try to return a read/write IndexIO instance if possible;
+   * otherwise it will return a read only instance.
    * 
-   * @param index The index object.
+   * @param index
+   *          The index object.
    * @return The most appropriate IndexIO implementation to use.
-   * @throws IOException If thrown by the constructors
+   * @throws IOException
+   *           If thrown by the constructors
    */
   public static IndexIO newInstance(Index index) throws IOException {
     Directory directory = index.getIndexDirectory();
     boolean readonly = false;
     // Detect if we can write on the files.
-    if (directory instanceof FSDirectory) {
-      File f = ((FSDirectory)directory).getFile();
-      readonly = !f.canWrite();
+    try {
+      if (directory instanceof FSDirectory) {
+        File f = ((FSDirectory) directory).getFile();
+        // ensure all files can write.
+        for (File tf : f.listFiles()) {
+          if (!tf.canWrite()) {
+            readonly = true;
+          }
+        }
+      } else {
+        readonly = true;
+      }
+    } catch (Exception e) {
+      readonly = true;
     }
-    // Returns the correct Index IO implementation
-    return readonly? new IndexIOReadOnly(index) : new IndexIOReadWrite(index);
-  }
 
+    // Returns the correct Index IO implementation
+    return readonly ? new IndexIOReadOnly(index) : new IndexIOReadWrite(index);
+  }
 }
