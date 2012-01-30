9334b56ea2502669cdb0e37813e0bddd1bff5708
tyenki <tyenki@gmail.com>
2012-01-30 16:20:57 +1100

    Code clean up

--- src/org/weborganic/flint/OpenIndexManager.java
+++ src/org/weborganic/flint/OpenIndexManager.java
@@ -21,68 +21,85 @@ import org.slf4j.LoggerFactory;
  * @author Jean-Baptiste Reure
  * @version 26 February 2010
  */
-public class OpenIndexManager {
+public final class OpenIndexManager {
+
   /**
    * Logger
    */
   private static final Logger LOGGER = LoggerFactory.getLogger(OpenIndexManager.class);
+
   /**
    * How to compare opened readers: the "highest" is the one that was used the least recently
    */
-  private final static Comparator<IndexIOReadWrite> openedIndexComparator = new Comparator<IndexIOReadWrite>() {
+  private static final Comparator<IndexIOReadWrite> OPEN_INDEX_COMPARATOR = new Comparator<IndexIOReadWrite>() {
+
     /**
      * {@inheritdoc}
      */
     public int compare(IndexIOReadWrite o1, IndexIOReadWrite o2) {
       return o1.getLastTimeUsed() < o2.getLastTimeUsed() ? 1 : o1.getLastTimeUsed() == o2.getLastTimeUsed() ? 0 : -1;
     }
+
   };
+
   /**
    * The list of all opened readers
    */
-  private static final ConcurrentHashMap<Integer, IndexIOReadWrite> openedIndexes = new ConcurrentHashMap<Integer, IndexIOReadWrite>();
+  private static final ConcurrentHashMap<Integer, IndexIOReadWrite> OPEN_INDEXES =
+    new ConcurrentHashMap<Integer, IndexIOReadWrite>();
+
+  /**
+   * Utility class.
+   */
+  private OpenIndexManager() {
+  }
+
   /**
    * The max number of opened reader allowed at all times
    */
-  private static int MAX_OPENED_INDEXES = 50;
+  private static int maxOpenedIndexes = 50;
+
   /**
    * @param val the new max number of opened reaer allowed at all times
    */
-  public final static void setMaxOpenedIndexes(int val) {
-    MAX_OPENED_INDEXES = val;
+  public static void setMaxOpenedIndexes(int val) {
+    maxOpenedIndexes = val;
   }
+
   /**
    * Go through all the opened readers and close the oldest ones until the number of
    * opened readers is less than the maximum allowed.
    */
-  public final static void closeOldReaders() {
+  public static void closeOldReaders() {
 //    LOGGER.debug("Currently {} opened reader(s): {}", openedIndexes.size(), openedIndexes);
-    while (openedIndexes.size() > MAX_OPENED_INDEXES) {
+    while (OPEN_INDEXES.size() > maxOpenedIndexes) {
       // get the oldest one
-      IndexIOReadWrite or = Collections.max(openedIndexes.values(), openedIndexComparator);
+      IndexIOReadWrite or = Collections.max(OPEN_INDEXES.values(), OPEN_INDEX_COMPARATOR);
       // ok try to close it
       try {
         LOGGER.debug("Closing index {}", or.hashCode());
         or.stop();
-      } catch (AlreadyClosedException e) {
+      } catch (AlreadyClosedException ex) {
         // good then
-      } catch (IndexException e) {
+      } catch (IndexException ex) {
         LOGGER.error("Failed closing an opened index {}", or.hashCode());
       }
     }
   }
+
   /**
    * @param index a new opened index to store
    */
   public static void add(IndexIOReadWrite index) {
     LOGGER.debug("Adding new index {}", index.hashCode());
-    openedIndexes.put(index.hashCode(), index);
+    OPEN_INDEXES.put(index.hashCode(), index);
   }
+
   /**
    * @param index the index to remove from the list
    */
   public static void remove(IndexIOReadWrite index) {
-    if (openedIndexes.remove(index.hashCode()) != null) {
+    if (OPEN_INDEXES.remove(index.hashCode()) != null) {
       LOGGER.debug("Removing index {}", index.hashCode());
     }
   }
