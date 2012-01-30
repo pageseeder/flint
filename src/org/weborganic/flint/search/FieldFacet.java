ba88c6f300b58682ace1a94c9ad68e364b76e65e
tyenki <tyenki@gmail.com>
2012-01-30 16:11:19 +1100

    Removed unused import

--- src/org/weborganic/flint/search/FieldFacet.java
+++ src/org/weborganic/flint/search/FieldFacet.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.Searcher;
 import org.apache.lucene.search.TermQuery;
