8c9c3803d6cb49769c4f04705cc9f4fc15c6387c
tyenki <devnull@localhost>
2012-02-13 15:35:47 +1100

    Code formatting

--- src/org/weborganic/flint/index/package-info.java
+++ src/org/weborganic/flint/index/package-info.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of the Flint library.
+ *
+ * For licensing information please see the file license.txt included in the release.
+ * A copy of this licence can also be found at
+ *   http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 /**
  * Handlers and classes mapping Lucene document model for indexing.
  */
