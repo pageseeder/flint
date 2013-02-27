1298415f9b6c0fc50bf57a57b9ca072834624d17
tyenki <tyenki@gmail.com>
2013-02-27 17:15:01 +1100

    Reverted to basic searcher manager to avoid reader being closed prematurely...

--- src/org/weborganic/flint/SearcherManager.java
+++ src/org/weborganic/flint/SearcherManager.java
@@ -9,7 +9,6 @@ package org.weborganic.flint;
 
 import java.io.IOException;
 
-import org.apache.lucene.index.FilterIndexReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.search.IndexSearcher;
@@ -33,7 +32,7 @@ import org.slf4j.LoggerFactory;
  * @author Jean-Baptiste Reure
  * @version 26 February 2010
  */
-final class SearcherManager {
+public class SearcherManager {
 
   /**
    * Logger
@@ -41,19 +40,9 @@ final class SearcherManager {
   private static final Logger LOGGER = LoggerFactory.getLogger(SearcherManager.class);
 
   /**
-   * The current near-real time reader used to run searches on the index
-   */
-  private SafeIndexReader _reader = null;
-
-  /**
    * The current searcher used to run searches on the index
    */
-  private IndexSearcher _searcher = null;
-
-  /**
-   * Flag used to set current state.
-   */
-  private volatile boolean reopening = false;
+  private IndexSearcher currentSearcher;
 
   /**
    * Create a new SearcherManager using the given writer.
@@ -62,9 +51,9 @@ final class SearcherManager {
    * @throws IOException If thrown while trying to get the reader.
    */
   public SearcherManager(IndexWriter writer) throws IOException {
-    // Lucene 3.1+: IndexReader.open(writer, true);
     IndexReader reader = writer.getReader();
-    set(reader);
+    // Lucene 3.1+: IndexReader.open(writer, true);
+    this.currentSearcher = new IndexSearcher(reader);
   }
 
   /**
@@ -74,24 +63,125 @@ final class SearcherManager {
    * @throws IOException If thrown while trying to get the reader.
    */
   public SearcherManager(IndexReader reader) throws IOException {
-    set(reader);
+    this.currentSearcher = new IndexSearcher(reader);
   }
 
-  // Getting and releasing index readers and searchers
-  // ----------------------------------------------------------------------------------------------
+  protected int getRefCount() {
+    return this.currentSearcher.getIndexReader().getRefCount();
+  }
+
+  // ------------------ Index re-opening methods -----------------------------
 
   /**
-   * Return the current IndexSearcher.
+   * flag used to set current state.
+   */
+  private boolean reopening = false;
+
+  /**
+   * Start re-open
+   */
+  private synchronized void startReopen() throws InterruptedException {
+    LOGGER.debug("Starting open");
+    while (this.reopening) {
+      wait();
+    }
+    this.reopening = true;
+  }
+
+  /**
+   * Conclude re-open.
+   */
+  private synchronized void doneReopen() {
+    LOGGER.debug("Finishing open");
+    this.reopening = false;
+    notifyAll();
+  }
+
+  /**
+   * Trigger a check to reopen the reader.
    *
-   * <p>Important: call release() when finished with the searcher.
+   * @throws InterruptedException
+   * @throws IOException
+   */
+  public void maybeReopen() throws InterruptedException, IOException {
+    startReopen();
+    try {
+      final IndexSearcher searcher = get();
+      try {
+        if (!this.currentSearcher.getIndexReader().isCurrent()) {
+          // if not current, we need to re-open it
+          IndexReader newReader = this.currentSearcher.getIndexReader().reopen();
+          IndexSearcher newSearcher = new IndexSearcher(newReader);
+          swapSearcher(newSearcher);
+        } else {
+          LOGGER.debug("Reader is still current so no need to re-open it");
+        }
+      } finally {
+        release(searcher);
+      }
+    } finally {
+      doneReopen();
+    }
+  }
+
+  /**
+   * Perform a swap between current searcher and given searcher.
+   *
+   * @param newSearcher
+   * @throws IOException
+   */
+  private synchronized void swapSearcher(IndexSearcher newSearcher) throws IOException {
+    LOGGER.debug("Swapping reader from {} to {}",
+        this.currentSearcher.getIndexReader().hashCode(), newSearcher.getIndexReader().hashCode());
+    release(this.currentSearcher);
+    this.currentSearcher = newSearcher;
+  }
+
+  /**
+   * Check if the reader provided is not current and not used anymore in which case it is closed.
+   *
+   * @param reader the reader to check
+   *
+   * @throws IOException if closing failed
+   */
+  private void closeIfDirty(IndexReader reader) throws IOException {
+    // check if we should close an old one
+    if (this.currentSearcher.getIndexReader() != reader) {
+      if (reader.getRefCount() == 0) {
+        LOGGER.debug("Closing reader {}", reader.hashCode());
+        try {
+          reader.close();
+        } catch (AlreadyClosedException e) {
+          // good then, no need to worry
+        }
+      } else {
+        LOGGER.debug("Cannot close reader {} as there are still references ({})", reader.hashCode(), reader.getRefCount());
+      }
+    }
+  }
+
+  /**
+   * Close this searcher by closing the current searcher and its current reader.
+   *
+   * @throws IOException If closing this searcher failed
+   */
+  protected final void close() throws IOException {
+    // close reader
+    this.currentSearcher.getIndexReader().close();
+    // then searcher
+    this.currentSearcher.close();
+  }
+
+  /**
+   * Return the current IndexSearcher. Important: call release() when finished with the searcher.
    *
    * @return the current IndexSearcher
    * @throws InterruptedException
    */
   protected synchronized IndexSearcher get() {
-    this._reader.incRef();
-    LOGGER.debug("Getting reader {}", this._reader.hashCode());
-    return this._searcher;
+    this.currentSearcher.getIndexReader().incRef();
+    LOGGER.debug("Getting reader {}", this.currentSearcher.getIndexReader().hashCode());
+    return this.currentSearcher;
   }
 
   /**
@@ -115,9 +205,9 @@ final class SearcherManager {
    * @throws InterruptedException
    */
   protected synchronized IndexReader getReader() {
-    LOGGER.debug("Getting reader {}", this._reader.hashCode());
-    this._reader.incRef();
-    return this._reader;
+    LOGGER.debug("Getting reader {}", this.currentSearcher.getIndexReader().hashCode());
+    this.currentSearcher.getIndexReader().incRef();
+    return this.currentSearcher.getIndexReader();
   }
 
   /**
@@ -134,133 +224,5 @@ final class SearcherManager {
     closeIfDirty(reader);
   }
 
-  protected int getRefCount() {
-    return this._reader.getRefCount() - 1;
-  }
-
-  // Index re-opening methods
-  // ----------------------------------------------------------------------------------------------
-
-  /**
-   * Trigger a check to reopen the reader.
-   *
-   * @throws InterruptedException
-   * @throws IOException
-   */
-  public void maybeReopen() throws InterruptedException, IOException {
-    startReopen();
-    try {
-      if (!this._reader.isCurrent()) {
-        // if not current, we need to re-open it
-        IndexReader newReader = this._reader.original().reopen();
-        LOGGER.debug("Swapping reader from {} to {}", this._reader.hashCode(), newReader.hashCode());
-        set(newReader);
-      } else {
-        LOGGER.debug("Reader is still current so no need to re-open it");
-      }
-    } finally {
-      doneReopen();
-    }
-  }
-
-  /**
-   * Start re-open
-   */
-  private synchronized void startReopen() throws InterruptedException {
-    LOGGER.debug("Starting open");
-    while (this.reopening) {
-      wait();
-    }
-    this.reopening = true;
-  }
-
-  /**
-   * Conclude re-open.
-   */
-  private synchronized void doneReopen() {
-    LOGGER.debug("Finishing open");
-    this.reopening = false;
-    notifyAll();
-  }
-
-  /**
-   * Set the real time reader and searchers to use.
-   *
-   * @param reader The reader to use.
-   */
-  private synchronized void set(IndexReader reader) {
-    this._reader = new SafeIndexReader(reader);
-    this._searcher = new IndexSearcher(reader);
-  }
-
-  /**
-   * Check if the reader provided is not current and not used anymore in which case it is closed.
-   *
-   * @param reader the reader to check
-   *
-   * @throws IOException if closing failed
-   */
-  private void closeIfDirty(IndexReader reader) throws IOException {
-    // check if we should close an old one
-    if (this._reader != reader && reader instanceof SafeIndexReader) {
-      // XXX: this condition may never occur
-      if (reader.getRefCount() == 0) {
-        LOGGER.debug("Closing reader {}", reader.hashCode());
-        try {
-          ((SafeIndexReader)reader).original().close();
-        } catch (AlreadyClosedException ex) {
-          // good then, no need to worry
-        }
-      } else {
-        LOGGER.debug("Cannot close reader {} as there are still references ({})", reader.hashCode(), reader.getRefCount());
-      }
-    }
-  }
-
-  /**
-   * Close this searcher by closing the current searcher and its current reader.
-   *
-   * @throws IOException If closing this searcher failed
-   */
-  protected final void close() throws IOException {
-    // close reader
-    this._reader.close();
-    // then searcher
-    this._searcher.close();
-  }
-
-  /**
-   * An index reader implementation which overrides some methods to protect the reader from
-   * being tempered with by different threads and therefore make it safe to share.
-   *
-   * @author Christophe Lauret
-   * @version 27 February 2013
-   */
-  private static final class SafeIndexReader extends FilterIndexReader {
-
-    /**
-     * Simply wraps the supplied index reader
-     *
-     * @param reader The reader to wrap.
-     */
-    public SafeIndexReader(IndexReader reader) {
-      super(reader);
-    }
-
-    @Override
-    protected void doClose() throws IOException {
-      StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
-      String caller = stacktrace.length > 0? stacktrace[0].toString() : null;
-      LOGGER.warn("{} tried to close the IndexReader for", caller);
-    }
-
-    /**
-     * @return the original Index reader used.
-     */
-    protected IndexReader original() {
-      return this.in;
-    }
-
-  }
 
 }
