b4108568b57277ad5e4756cc3ca995a5746a8bc1
jbreure <jbreure@weborganic.com>
2016-08-01 12:11:09 +1000

    Handle case where a query does not support weights

--- src/main/java/org/pageseeder/flint/query/SearchResults.java
+++ src/main/java/org/pageseeder/flint/query/SearchResults.java
@@ -305,23 +305,31 @@ public final class SearchResults implements XMLWritable {
     xml.openElement("documents", true);
 
     // Iterate over the hits to find the extracts
+    boolean withExtracts = true;
     for (int i = firsthit - 1; i < lasthit; i++) {
       String score = Float.toString(this._scoredocs[i].score);
       Document doc = this._searcher.doc(this._scoredocs[i].doc);
       String extractXML = null;
-      Set<Term> terms = new HashSet<Term>();
-      this._searcher.createWeight(this._query.toQuery(), true).extractTerms(terms);
-      for (IndexableField f : doc.getFields()) {
+      if (withExtracts) {
+        Set<Term> terms = new HashSet<Term>();
+        try {
+          this._searcher.createWeight(this._query.toQuery(), true).extractTerms(terms);
+        } catch (UnsupportedOperationException ex) {
+          // The query provided does not support weight, can't have extracts then
+          withExtracts = false;
+        }
         for (Term t : terms) {
-          if (t.field().equals(f.name())) {
-            String extract = Documents.extract(Fields.toString(f), t.text(), 200);
-            if (extract != null) {
-              XMLStringWriter xsw = new XMLStringWriter(false);
-              xsw.openElement("extract");
-              xsw.attribute("from", t.field());
-              xsw.writeXML(extract);
-              xsw.closeElement();
-              extractXML = xsw.toString();
+          for (IndexableField f : doc.getFields()) {
+            if (t.field().equals(f.name())) {
+              String extract = Documents.extract(Fields.toString(f), t.text(), 200);
+              if (extract != null) {
+                XMLStringWriter xsw = new XMLStringWriter(false);
+                xsw.openElement("extract");
+                xsw.attribute("from", t.field());
+                xsw.writeXML(extract);
+                xsw.closeElement();
+                extractXML = xsw.toString();
+              }
             }
           }
         }
